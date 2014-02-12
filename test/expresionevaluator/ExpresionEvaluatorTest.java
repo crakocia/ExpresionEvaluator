d366d01bc603c992cb12993eb15aaeeacf2bf3ab
Víctor Pérez Santana <crakocia@gmail.com>
2014-02-12 09:11:43 +0000

    Create Addtion Test

--- test/expresionevaluator/ExpresionEvaluatorTest.java
+++ test/expresionevaluator/ExpresionEvaluatorTest.java
@@ -15,12 +15,19 @@ public class ExpresionEvaluatorTest {
     }
     
     @Test
-    public void plusTest(){
+    public void AdditionTest(){
         ExpresionEvaluator expEval = new ExpresionEvaluator();
         
         Assert.assertEquals(4, expEval.evalExpresion("2+2"));
         Assert.assertEquals(5, expEval.evalExpresion("2+3"));
         Assert.assertEquals(9, expEval.evalExpresion("2+3+4"));    
         Assert.assertEquals(16, expEval.evalExpresion("4+4+8"));
+    }
+    
+    @Test
+    public void MultiplicationTest(){
+        ExpresionEvaluator expEval = new ExpresionEvaluator();
+        
+        Assert.assertEquals(4, expEval.evalExpresion("1+2*3"));        
     }
 }
