e717531c31429e99f195357e6f3599f9b645d982
Víctor Pérez Santana <crakocia@gmail.com>
2014-02-12 08:56:00 +0000

    Commit 12/02/14

--- test/expresionevaluator/ExpresionEvaluatorTest.java
+++ test/expresionevaluator/ExpresionEvaluatorTest.java
@@ -20,6 +20,7 @@ public class ExpresionEvaluatorTest {
         
         Assert.assertEquals(4, expEval.evalExpresion("2+2"));
         Assert.assertEquals(5, expEval.evalExpresion("2+3"));
-        Assert.assertEquals(9, expEval.evalExpresion("2+3+4"));        
+        Assert.assertEquals(9, expEval.evalExpresion("2+3+4"));    
+        Assert.assertEquals(16, expEval.evalExpresion("4+4+8"));
     }
 }
